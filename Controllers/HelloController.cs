9845c15f3057043ed681bfd6ee0a93a3185a3809
ngo pham anh huy <huynpase1@gmail.com>
2019-09-19 17:01:22 +0900

    First commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
